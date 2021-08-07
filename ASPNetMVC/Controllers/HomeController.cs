d7bfffe1e503bea8ebbc4f473ecda49b53ea08e2
sbstleal <sebastiaodeoleal@gmail.com>
2021-08-07 16:09:35 -0300

    MVC

---
+++ ASPNetMVC/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using atividadeAlunoMedia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPNetMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            ViewBag.Blabla = "Bla blaage.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Alunos()
+        {
+            List<Aluno> alunos = new List<Aluno>();
+            var aluno = new Aluno()
+            {
+                Id = 1,
+                Nome = "Danilo",
+                Matricula = "001"
+            };
+            aluno.Notas.Add(9);
+            aluno.Notas.Add(10);
+
+            alunos.Add(aluno);
+
+            ViewBag.Alunos = alunos;
+
+            return View();
+        }
+    }
+}
